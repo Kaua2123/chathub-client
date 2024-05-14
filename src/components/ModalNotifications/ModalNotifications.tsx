56558d32df1e5d0b6d921517ca2067fdc870060a
Kaua2123 <kauantnetsoares@gmail.com>
2024-05-14 11:58:05 -0300

    feat: Permite drag em notificações

--- src/components/ModalNotifications/ModalNotifications.tsx
+++ src/components/ModalNotifications/ModalNotifications.tsx
@@ -1,3 +1,4 @@
+import { DndContext, DragEndEvent, closestCenter } from '@dnd-kit/core';
 import NotificationCard from '../NotificationCard/NotificationCard';
 import {
   CircleXIcon,
@@ -10,6 +11,12 @@ import {
   DeleteButton,
   DivNotifications,
 } from './styled';
+import {
+  SortableContext,
+  arrayMove,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import { useState } from 'react';
 
 export type ModalNotificationsProps = {
   setIsSeeingNotification: React.Dispatch<React.SetStateAction<boolean>>;
@@ -18,6 +25,27 @@ export type ModalNotificationsProps = {
 function ModalNotifications({
   setIsSeeingNotification,
 }: ModalNotificationsProps) {
+  const [notifications, setNotifications] = useState([1, 2, 3, 4]);
+
+  function handleDragEnd(e: DragEndEvent) {
+    const { active, over } = e;
+
+    if (!over) return;
+
+    if (active.id !== over.id) {
+      setNotifications((notifications) => {
+        const oldIndex = notifications.indexOf(active.id as number);
+        const newIndex = notifications.indexOf(over.id as number);
+
+        localStorage.setItem(
+          'notifications',
+          JSON.stringify(arrayMove(notifications, oldIndex, newIndex)),
+        );
+        return arrayMove(notifications, oldIndex, newIndex);
+      });
+    }
+  }
+
   return (
     <div>
       <Modal>
@@ -36,9 +64,19 @@ function ModalNotifications({
             </CloseButton>
           </Div>
           <DivNotifications>
-            {Array.from({ length: 3 }).map((_, i) => {
-              return <NotificationCard key={i} />;
-            })}
+            <DndContext
+              onDragEnd={(e) => handleDragEnd(e)}
+              collisionDetection={closestCenter}
+            >
+              <SortableContext
+                items={notifications}
+                strategy={verticalListSortingStrategy}
+              >
+                {notifications.map((_, i) => {
+                  return <NotificationCard key={i} id={i} />;
+                })}
+              </SortableContext>
+            </DndContext>
           </DivNotifications>
         </Container>
       </Modal>
--- src/components/NotificationCard/NotificationCard.tsx
+++ src/components/NotificationCard/NotificationCard.tsx
@@ -1,7 +1,25 @@
+import { useSortable } from '@dnd-kit/sortable';
 import { Div } from './styled';
+import { CSS } from '@dnd-kit/utilities';
 
-function NotificationCard() {
-  return <Div>Você recebeu um pedido de amizade de username201</Div>;
+export type NotificationCardProps = {
+  id: number;
+};
+
+function NotificationCard({ id }: NotificationCardProps) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <Div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      Você recebeu um pedido de amizade de username201
+    </Div>
+  );
 }
 
 export default NotificationCard;
