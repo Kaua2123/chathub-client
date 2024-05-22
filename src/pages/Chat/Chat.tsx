c0ec7c0e26926cd074e91946f241b9e7879e434e
Kaua2123 <kauantnetsoares@gmail.com>
2024-05-22 10:17:44 -0300

    feat: Torna Chat um componente só para maior simplificação de integração de websockets

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,14 +1,64 @@
-import ChatBar from '../../components/ChatBar/ChatBar';
-import ChatBottomInputMessage from '../../components/ChatBottomInputMessage/ChatBottomInputMessage';
+import { useEffect, useState } from 'react';
+import {
+  ArrowLeftIcon,
+  Button,
+  Container,
+  Div,
+  DivConfig,
+  DivUser,
+  Input,
+  SendHorizontalIcon,
+  TopContainer,
+  TrashIcon,
+  UserAvatar,
+} from './styled';
+import { socket } from '../../socket';
+import ModalDeleting from '../../components/ModalDeleting/ModalDeleting';
+import { User } from 'lucide-react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 function Chat() {
-  //   const { id } = useParams();
+  const { id } = useParams();
+  const [socketInstance] = useState(socket());
+  const navigate = useNavigate();
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  useEffect(() => {
+    // sempre que chegar uma mensagem, esse evento é chamado
+    socketInstance.on('msg', (msg: string) => {
+      console.log('Msg sent:', msg);
+    });
+  });
 
   return (
     <div>
-      <ChatBar />
+      {isDeleting && <ModalDeleting setIsDeleting={setIsDeleting} />}
+      <TopContainer>
+        <DivUser>
+          <ArrowLeftIcon
+            onClick={() => {
+              navigate('/conversations');
+            }}
+          />
+          <UserAvatar>
+            <User color="black" />
+          </UserAvatar>
+          <p>username user_id: {id}</p>
+        </DivUser>
+        <DivConfig>
+          <TrashIcon onClick={() => setIsDeleting(true)} />
+        </DivConfig>
+      </TopContainer>
 
-      <ChatBottomInputMessage />
+      <Container>
+        <Div>
+          <Input type="text" placeholder="Envie algo" />
+
+          <Button>
+            <SendHorizontalIcon color="black" />
+          </Button>
+        </Div>
+      </Container>
     </div>
   );
 }
---
+++ src/pages/Chat/styled.ts
@@ -0,0 +1,131 @@
+import {
+  SendHorizontal,
+  ArrowLeft,
+  EllipsisVertical,
+  Trash,
+} from 'lucide-react';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: absolute;
+  bottom: 0;
+  color: ${({ theme }) => theme.colors.white};
+  padding: 2rem;
+  border-top-left-radius: 2rem;
+  border-top-right-radius: 2rem;
+  border: 1px solid ${({ theme }) => theme.colors.lightBlue};
+  font-family: 'Raleway';
+  width: 100%;
+`;
+
+export const Div = styled.div`
+  display: flex;
+  padding-left: 1.5rem;
+  gap: 2rem;
+`;
+
+export const Button = styled.button`
+  border: 1px solid white;
+  background-color: ${({ theme }) => theme.colors.white};
+  padding: 1rem;
+  border-radius: 100%;
+  display: flex;
+  align-items: center;
+  transition: transform 0.3s;
+  cursor: pointer;
+
+  &:hover {
+    transform: translateY(-5%);
+
+    & svg {
+      // stroke para alterar a cor das linhas do ícone
+      stroke: ${({ theme }) => theme.colors.lighterBlue};
+    }
+  }
+
+  border: none;
+  cursor: pointer;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.mediumBlue};
+  color: ${({ theme }) => theme.colors.darkGray};
+  font-family: 'Raleway';
+  font-size: 1.8rem;
+  outline: none;
+  border: 1px solid ${({ theme }) => theme.colors.darkBlue};
+  transition: transform 0.3s;
+  border-radius: 0.9rem;
+  padding-left: 4rem;
+
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.colors.lighterBlue};
+  }
+`;
+
+export const SendHorizontalIcon = styled(SendHorizontal)``;
+
+export const TopContainer = styled.div`
+  color: ${({ theme }) => theme.colors.white};
+  padding: 2rem;
+  border-bottom-left-radius: 2rem;
+  border-bottom-right-radius: 2rem;
+  border: 1px solid ${({ theme }) => theme.colors.lightBlue};
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  gap: 4rem;
+  font-family: 'Raleway';
+`;
+
+export const UserAvatar = styled.div`
+  border: 1px solid white;
+  background-color: ${({ theme }) => theme.colors.white};
+  padding: 1rem;
+  border-radius: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+export const EllipsisVerticalIcon = styled(EllipsisVertical)`
+  transition: transform 0.3s;
+  cursor: pointer;
+
+  &:hover {
+    transform: translateY(-10%);
+    color: ${({ theme }) => theme.colors.lighterBlue};
+  }
+`;
+export const ArrowLeftIcon = styled(ArrowLeft)`
+  transition: transform 0.3s;
+  cursor: pointer;
+
+  &:hover {
+    transform: translateX(-10%);
+    color: ${({ theme }) => theme.colors.lighterBlue};
+  }
+`;
+
+export const DivUser = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  gap: 1.4rem;
+`;
+export const DivConfig = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const TrashIcon = styled(Trash)`
+  transition: transform 0.3s;
+  z-index: 2;
+  cursor: pointer;
+
+  &:hover {
+    color: #c67c7c;
+
+    transform: translateY(-10%);
+  }
+`;
